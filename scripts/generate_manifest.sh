857d78d31d33266cb2953811d3f76e7a1516b38f
scriptonist <scriptonist@gmail.com>
2022-11-10 18:09:25 +0530

    ci: fix manifest generation, add version

--- scripts/generate_manifest.sh
+++ scripts/generate_manifest.sh
@@ -6,19 +6,26 @@ set -evo pipefail
 
 BASE_DIR="$1"
 TEMPLATE_PATH="$2"
+VERSION="$3"
 
-HEAD_URL='https://github.com/scriptonist/hasura-cli-track/releases/download';
+HEAD_URL="https://github.com/scriptonist/hasura-cli-track/releases/download/${VERSION}";
 
-LINUX_AMD64_ASSET="${BASE_DIR}/hasura-cli-track-linux-amd64.tar.gz" 
-MACOS_AMD64_ASSET="${BASE_DIR}/hasura-cli-track-macos-amd64.tar.gz"
-WINDOWS_AMD64_ASSET="${BASE_DIR}/hasura-cli-track-windows-amd64.exe.zip"
+LINUX_AMD64_ASSET="hasura-cli-track-linux-amd64.tar.gz" 
+MACOS_AMD64_ASSET="hasura-cli-track-macos-amd64.tar.gz"
+WINDOWS_AMD64_ASSET="hasura-cli-track-windows-amd64.exe.zip"
 
-LINUX_AMD64_ASSET_SHA256=$(cat ${LINUX_AMD64_ASSET}.sha256 | awk '{print $1}')
-MACOS_AMD64_ASSET_SHA256=$(cat ${MACOS_AMD64_ASSET}.sha256 | awk '{print $1}')
-WINDOWS_AMD64_ASSET_SHA256=$(cat ${WINDOWS_AMD64_ASSET}.sha256 | awk '{print $1}')
+LINUX_AMD64_ASSET_LOCATION="${BASE_DIR}/hasura-cli-track-linux-amd64.tar.gz" 
+MACOS_AMD64_ASSET_LOCATION="${BASE_DIR}/hasura-cli-track-macos-amd64.tar.gz"
+WINDOWS_AMD64_ASSET_LOCATION="${BASE_DIR}/hasura-cli-track-windows-amd64.exe.zip"
+
+LINUX_AMD64_ASSET_SHA256=$(cat "${LINUX_AMD64_ASSET_LOCATION}".sha256 | awk '{print $1}')
+MACOS_AMD64_ASSET_SHA256=$(cat "${MACOS_AMD64_ASSET_LOCATION}".sha256 | awk '{print $1}')
+WINDOWS_AMD64_ASSET_SHA256=$(cat "${WINDOWS_AMD64_ASSET_LOCATION}".sha256 | awk '{print $1}')
 
 
 yq "
+  .version = \"${VERSION}\" |
+
   .platforms[0].uri = \"$HEAD_URL/$LINUX_AMD64_ASSET\" |
   .platforms[0].sha256 = \"$LINUX_AMD64_ASSET_SHA256\" |
 
